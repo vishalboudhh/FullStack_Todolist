ba0520cb0ab25bc136b26abbabf60c51ba830dc7
vishal meshram <107854847+vishalboudhh@users.noreply.github.com>
2025-04-01 12:08:52 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,21 @@ import userRouter from './routes/user.routes.js';
 import todoRouter from './routes/todo.routes.js';
 import cookieParser from 'cookie-parser';
 import cors from "cors"
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serve frontend from server
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
+});
 
 //.env config
 dotenv.config();
@@ -18,11 +32,11 @@ connectDB();
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
-// app.use(cors({
-//     origin:"http://localhost:5173",
-//     credentials:true
-// }))
-app.use(cors());
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 //routes
 app.use('/api/v1/user',userRouter);
